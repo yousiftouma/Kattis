de740cc52c75d0890b03f921c0f71292f8110bcb
yousiftouma <yousif_touma@hotmail.com>
2015-09-21 23:12:40 +0200

    update content of kattis b

---
+++ kattis/KattisB/KattisB/Source.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main() {
+	int N;
+	int M;
+	set<int> jacks;
+
+	while (true) {
+		int res = 0;
+		cin >> N >> M;
+		if (N == 0 && M == 0) {
+			break;
+		}
+		int cd;
+		while (N > 0) {
+			cin >> cd;
+			jacks.insert(cd);
+			--N;
+		}
+		while (M > 0) {
+			cin >> cd;
+			jills.insert(cd);
+			--M;
+		}
+
+		for (int cd : jacks) {
+			if (jills.find(cd) != jills.end())
+				++res;
+		}
+		cout << res << endl;
+
+	}
+
+	return 0;
+}
