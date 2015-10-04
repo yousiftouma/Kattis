82b6295ab5d7428b9a7fc99846dd32dfcb3925c6
Yousif Touma <youto814@student.liu.se>
2015-10-04 12:15:38 +0200

    C success

--- kattis/KattisC/KattisC/Source.cpp
+++ kattis/KattisC/KattisC/Source.cpp
@@ -5,46 +5,41 @@ using namespace std;
 int main() {
 	int inputs;
 	int queries;
-	int cases = 0;
+	auto cases = 0;
 	int size;
-	int sumsSize;
 	long input;
 	long query;
 	long res;
-	long sum;
+	size_t i;
+	size_t j;
 	vector<long> numbers;
-	set<long> sums;
-	while (true) {
+	vector<long> sums;
+	while (cin >> inputs) {
 		numbers.clear();
 		sums.clear();
-		cin >> inputs;
-		size = inputs;
 		++cases;
 		cout << "Case " << cases << ":" << endl;
-		for (inputs; inputs > 0; --inputs) {
+		for (size = inputs; size > 0; --size) {
 			cin >> input;
 			numbers.push_back(input);
 		}
-		for (int i = 0; i < size - 1; ++i) {
-			for (int j = i + 1; j < size; ++j) {
-				sums.insert(numbers[i] + numbers[j]);
+		for (i = 0; i < inputs - 1; ++i) {
+			for (j = i + 1; j < inputs; ++j) {
+				sums.push_back(numbers[i] + numbers[j]);
 			}
 		}
-		sumsSize = sums.size();
 		cin >> queries;
 		for (queries; queries > 0; --queries) {
 			cin >> query;
 			res = *sums.begin();
 			for (auto f : sums) {
-				cout << "currsum = " << f << endl;
 				if (abs(f - query) < abs(res - query)) {
 					res = f;
 				}
-				else if (!(res == f)) break;
+				//else if (res != f) break;
 			}
 			cout << "Closest sum to " << query << " is " << res << "." << endl;
 		}
 	}
-
 	return 0;
 }
