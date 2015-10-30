a0ffb421f877f896485f766e30914bcc0a34a5fb
yousiftouma <yousif_touma@hotmail.com>
2015-10-30 13:31:25 +0100

    progress kattis g j

--- kattis/KattisG/KattisG/Source.cpp
+++ kattis/KattisG/KattisG/Source.cpp
@@ -1,7 +1,57 @@
 #include <iostream>
 #include <queue>
+#include <stack>
+using namespace std;
 
 int main()
 {
+	freopen("output.txt", "w", stdout);
+	int commands, op, operand;
+	int isQ, isS, isPq;
+	while (cin >> commands)
+	{
+		queue<int> que;
+		stack<int> stack;
+		priority_queue<int> prioQue;
+		isQ = 1;
+		isPq = 1;
+		isS = 1;
+		while (commands--)
+		{
+			cin >> op >> operand;
+			if (op == 1)
+			{
+				if (isQ) que.push(operand);
+				if (isS) stack.push(operand);
+				if (isPq) prioQue.push(operand);
+			}
+			else
+			{
+				if(isQ) {
+					if (que.empty() || que.front() != operand) isQ = 0;
+					else que.pop();
+				}
+				
+				if(isS)
+				{
+					if (stack.empty() || stack.top() != operand) isS = 0;
+					else stack.pop();
+				}
+				
+				if (isPq)
+				{
+					if (prioQue.empty() || prioQue.top() != operand) isPq = 0;
+					else prioQue.pop();
+				}	
+			}
+		}
+		auto sum = isQ + isS + isPq;
+		if (sum > 1) cout << "not sure" << endl;
+		else if (sum == 0) cout << "impossible" << endl;
+		else if (isQ == 1) cout << "queue" << endl;
+		else if (isS == 1) cout << "stack" << endl;
+		else if (isPq == 1) cout << "priority queue" << endl;
+	}
+
 	return 0;
 }
--- kattis/KattisJ/KattisJ/Source.cpp
+++ kattis/KattisJ/KattisJ/Source.cpp
@@ -1,55 +1,121 @@
 #include <iostream>
+#include <array>
 using namespace std;
+
+void merge(int a[], const int low, const int mid, const int high)
+{
+	// Variables declaration. 
+	int * b = new int[high + 1 - low];
+	int h, i, j, k;
+	h = low;
+	i = 0;
+	j = mid + 1;
+	// Merges the two array's into b[] until the first one is finished
+	while ((h <= mid) && (j <= high))
+	{
+		if (a[h] <= a[j])
+		{
+			b[i] = a[h];
+			h++;
+		}
+		else
+		{
+			b[i] = a[j];
+			j++;
+		}
+		i++;
+	}
+	// Completes the array filling in it the missing values
+	if (h>mid)
+	{
+		for (k = j;k <= high;k++)
+		{
+			b[i] = a[k];
+			i++;
+		}
+	}
+	else
+	{
+		for (k = h;k <= mid;k++)
+		{
+			b[i] = a[k];
+			i++;
+		}
+	}
+	// Prints into the original array
+	for (k = 0;k <= high - low;k++)
+	{
+		a[k + low] = b[k];
+	}
+	delete[] b;
+}
+
+void merge_sort(int a[], const int low, const int high)		// Recursive sort ...
+{
+	int mid;
+	if (low < high)
+	{
+		mid = (low + high) / 2;
+		merge_sort(a, low, mid);
+		merge_sort(a, mid + 1, high);
+		merge(a, low, mid, high);
+	}
+}
+
+
+int binary_srch_ret_index(int inp[], int e, int low, int high) {
+
+	if (low > high) {
+		return -1;
+	}
+
+	int mid = (low + high) / 2;
+
+	if (e == inp[mid]) {
+		return mid;
+	}
+
+	if (e < inp[mid]) {
+		return binary_srch_ret_index(inp, e, low, mid - 1);
+	}
+	else {
+		return binary_srch_ret_index(inp, e, mid + 1, high);
+	}
+}
+
+
+
 
 int main()
 {
 	auto students = new int[1000000];
 	int numberOfStudents, temp;
 	cin >> numberOfStudents;
-	cin >> students[0];
-	auto swaps = 0;
-
-	for (auto i = 1; i < numberOfStudents; ++i)
+	int inversions = 0;
+	for (auto i = 0; i < numberOfStudents; ++i)
 	{
 		cin >> students[i];
-/*		for (auto k = i - 1; k >= 0; --k)
-		{
-			if (students[k] > students[i])
-			{
-				temp = students[k];
-				students[k] = students[i];
-				students[i] = temp;
-				++swaps;
-			}
-		}*/
 	}
 
-	auto gap = numberOfStudents;
-	auto shrink = 1.3;
-	bool swapped = true;
-	while (gap != 1 || swapped == true)
+	auto unsorted = new int[1000000];
+	for (auto i = 0; i < numberOfStudents; ++i)
 	{
-		gap = gap / shrink;
-		if (gap < 1) gap = 1;
-		int i = 0;
-		swapped = false;
-		while (i + gap < numberOfStudents)
+		unsorted[i] = students[i];
+	}
+
+	merge_sort(students, 0, numberOfStudents);
+	
+	for (auto i = 0; i < numberOfStudents; ++i)
+	{
+		auto index = binary_srch_ret_index(students, unsorted[i], 0, numberOfStudents - i - 1);
+		inversions += index;
+		for (auto k = index; k < numberOfStudents - i; ++k)
 		{
-			if (students[i] > students[i + gap])
-			{
-				temp = students[i];
-				students[i] = students[i + gap];
-				students[i + gap] = temp;
-				swapped = true;
-				swaps += 1;
-				
-			}
-			++i;
+			students[k] = students[k + 1];
 		}
 	}
 
-	
-	cout << swaps << endl;
-	delete students;
+	cout << inversions << endl;
+	//delete students, unsorted;
 	return 0;
 }
